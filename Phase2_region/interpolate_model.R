79dc77f00257ea233d05b874c7064cc60123fbc1
Christy Rollinson <crollinson@gmail.com>
2017-05-09 12:57:45 -0500

    Fix interpolation
    Somewhere along the way the interp function started returning a list
    rather than a data frame, so that had to be fixed.

--- Phase2_region/interpolate_model.R
+++ Phase2_region/interpolate_model.R
@@ -17,18 +17,27 @@ interp.mod <- function(dat.all, mod.missing, mod.mask, var.interp){
   for(x in unique(dat.all[dat.all$Model==mod.missing,"Time"])){
     dat.interp <- SpatialPointsDataFrame(coords=dat.all[!is.na(dat.all[,var.interp]) & dat.all$Model==mod.missing & dat.all$Time==x ,c("lon", "lat")], data=dat.all[!is.na(dat.all[,var.interp]) & dat.all$Model==mod.missing & dat.all$Time==x,])
     
-    interp.out <- data.frame(interp(x=dat.interp, z=var.interp,
-                                    xo=lon, nx=length(lon),
-                                    yo=lat, ny=length(lat),
-                                    linear=T, extrap=F))
+    # Somewheres along the way, this started returning a list rather than a data frame
+    interp.list <- interp(x=dat.interp, z=var.interp,
+                          xo=lon, nx=length(lon),
+                          yo=lat, ny=length(lat),
+                          linear=T, extrap=F)
+    
+    interp.out <- data.frame(lon=interp.list$x, 
+                             lat=rep(interp.list$y, each=length(interp.list$x)), 
+                             z=stack(data.frame(interp.list$z))[,1])
+    # interp.out <- data.frame(interp(x=dat.interp, z=var.interp,
+    #                                 xo=lon, nx=length(lon),
+    #                                 yo=lat, ny=length(lat),
+    #                                 linear=T, extrap=F))
 
     for(i in unique(dat.all[!is.na(dat.all[,var.interp]) & dat.all$Model==mod.mask,"lat"])){
       for(j in unique(dat.all[dat.all$lat==i & !is.na(dat.all[,var.interp]) & dat.all$Model==mod.mask,"lon"])){
         if(!is.na(dat.all[dat.all$lat==i & dat.all$lon==j & dat.all$Model==mod.missing & dat.all$Time==x,var.interp])) next
-        # find closest interpolated grid cell
+        # find closest interpolated grid cell(s)
         use <- which(sqrt((j-interp.out$lon)^2 + (i-interp.out$lat)^2)==min(sqrt((j-interp.out$lon)^2 + (i-interp.out$lat)^2))) # Entering the 
         dat.all[dat.all$lat==i & dat.all$lon==j & dat.all$Model==mod.missing & dat.all$Time==x,"flag"] <- "interpolated"
-        dat.all[dat.all$lat==i & dat.all$lon==j & dat.all$Model==mod.missing & dat.all$Time==x,var.interp] <- mean(interp.out[use,var.interp])
+        dat.all[dat.all$lat==i & dat.all$lon==j & dat.all$Model==mod.missing & dat.all$Time==x,var.interp] <- mean(interp.out[use,"z"]) # Mean helps if there's >1 cell of equal distance
       }
     }
   }
